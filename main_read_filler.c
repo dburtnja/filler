098c66bf3c3656f4b4198f72aa1b27d0433fa383
dburtnja <dostup.te@gmail.com>
2017-03-18 12:35:14 +0200

    add read_filler

---
+++ main_read_filler.c
@@ -0,0 +1,20 @@
+//
+// Created by Denys on 18.03.2017.
+//
+
+#include "filler.h"
+
+int	main(void)
+{
+	int fd;
+	char *buf;
+
+	fd = open("from_filler.txt", O_RDWR | O_TRUNC | O_CREAT, 0777);
+	while (get_next_line(0, &buf) > 0)
+	{
+		ft_putendl_fd(buf, fd);
+		ft_strdel(&buf);
+	}
+	close(fd);
+	return (0);
+}
