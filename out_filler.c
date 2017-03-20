e4ba0206fcbf2b176947b58d9a20d40ae291b5a4
Деніс <denys@192.168.174.128.local.ipnet.ua>
2017-03-20 09:55:17 +0200

    add

--- out_filler.c
+++ out_filler.c
@@ -7,11 +7,26 @@
 int		main(int argc, char **argv)
 {
 	char	*buf;
+	int		i;
 
 	while (get_next_line(0, &buf) > 0)
 	{
 		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
+		ft_putendl(buf);
 		ft_strdel(&buf);
+	i = 100000;
+		while (i > 0)
+			i--;
 		ft_putstr("\033[2J");
 		ft_putstr("\033[0;0f");
 	}
