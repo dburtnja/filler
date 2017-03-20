ea094192e762ffd523d3e7a95b237483561b748a
dburtnja <dostup.te@gmail.com>
2017-03-20 08:37:41 +0200

    filler 1.2, add out_filler

---
+++ out_filler.c
@@ -0,0 +1,18 @@
+//
+// Created by Denys on 20.03.2017.
+//
+
+#include "filler.h"
+
+int		main(int argc, char *argv)
+{
+	char	*buf;
+
+	while (get_next_line(0, &buf) > 0)
+	{
+		ft_putendl(buf);
+		ft_strdel(&buf);
+		clear();
+	}
+	return (0);
+}
